09aff2655a602519ea9a3b32252e02a0f283baaa
THOMAS-KILLER-HD <42076641+THOMAS-KILLER-HD@users.noreply.github.com>
2019-05-05 13:54:21 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client(574539670617325588);
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
