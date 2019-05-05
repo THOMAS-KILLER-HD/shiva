88c692fad711f1178d4a4c6d4cd25c1146dbe9e0
THOMAS-KILLER-HD <42076641+THOMAS-KILLER-HD@users.noreply.github.com>
2019-05-05 13:09:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client(574539670617325588);
 
 client.on('ready', () => {
     console.log('I am ready!');
